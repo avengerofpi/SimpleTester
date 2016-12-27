5bcabb0082f3996341e7ad8dd25f521686d530f7
Cory Washington <tbd>
2016-12-27 01:43:03 -0500

    More sophisticated testing logic
    Added a hard-coded default max test length value, so that if the
    properties file parsed gives more than that many properties the user
    gets a chance to choose a smaller value for this exam. The default max
    is 50; this is b/c I don't currently want the prompt to show up when
    operating against stateCaptials.props (which has 50 prompts).
    
    The testing loop now has an outer loop that lets the user retest against
    the entire set of prompts just tested against. This loop occurs after
    the test size has already been chosen and the random prompts/properties
    list has been selected.
    
    So, for example, if run against the courtryCapitals.props file, which
    has more than 50 prompts, the user gets a chance to choose a different
    test size than the entire list. Maybe the user chooses 10. Then 10 of
    the prompts are randomly selected and the user is tested against these.
    Upon successful completion of this test the user can choose to retest
    against this same set of 10 prompts or else exit.

--- simpleTester.java
+++ simpleTester.java
@@ -8,37 +8,94 @@ import java.util.ArrayList;
 import java.util.Properties;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.InputMismatchException;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
 
 public class simpleTester {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        
-        String propsFileName = "/home/cory/git/stateCapitals/stateCapitals.props";
-        Properties stateCapitalsProps = new Properties();
-        File propsFile = new File(propsFileName);
-        Scanner reader = new Scanner(System.in);
-        
-        System.out.println(propsFileName);
+        // Setup a random number generator for later use
+        Random rand = new Random();
 
+        // Easy access to print and println
+        
+        
+        // Variables for interacting with the testing list(s)
+        int size;
+        int randIndex;
+        String state, capital;
+        String repeat;
+
+        // Choose the properties file to use for the test
+        String propsFileName;
+        propsFileName = "/home/cory/git/stateCapitals/stateCapitals.props";
+        propsFileName = "/home/cory/git/stateCapitals/countryCapitals.props";
+        
+        // Make sure the chosen test property file exists
+        File propsFile = new File(propsFileName);
         if (!propsFile.exists()) {
             String errorMsg = "ERROR: Could not find the chosen properties file: " + propsFileName;
             System.out.println(errorMsg);
             int ignoreMe = System.in.read();
             System.out.println("" + ignoreMe);
-            reader.close();
-            return;
+            System.exit(0);
         }
+
+        // Choose how to interact with the user (e.g., using java.util.Scanner)
+        Scanner reader = new Scanner(System.in);
         
+        // Import the test property file and declare/initialize some useful lists
+        Properties stateCapitalsProps = new Properties();
         stateCapitalsProps.load(new FileInputStream(propsFile));
-        final ArrayList<String> states = new ArrayList<>(stateCapitalsProps.stringPropertyNames());
+        final ArrayList<String> originalFullTestList = new ArrayList<>(stateCapitalsProps.stringPropertyNames());
+        ArrayList<String> fullTestList;
         ArrayList<String> successList = new ArrayList<>();
-        ArrayList<String> failureList = new ArrayList<>(states);
+        ArrayList<String> failureList = new ArrayList<>(originalFullTestList);
+        final ArrayList<String> trimmedFailureList;
         ArrayList<String> statesThisRound;
         
-        Random rand = new Random();
-        int size;
-        int randIndex;
-        String state, capital;
+        // Deciding on test size
+        final int DEFAULT_TEST_SIZE_MAX = 50;
+        final int originalFullTestSize = originalFullTestList.size();
+        int testSize = originalFullTestSize;
+        if (DEFAULT_TEST_SIZE_MAX < testSize) {
+            System.out.println("There are " + testSize + " prompts to test.");
+            System.out.println("Do you want to choose a smaller number for this test?");
+            System.out.print("  yes or no (will default to no): ");
+            String choice = reader.nextLine();
+            if (choice.toLowerCase().equals("yes")) {
+                System.out.print("How large a list do you want (will default to " + DEFAULT_TEST_SIZE_MAX + " on invalid choice): ");
+                try {
+                    testSize = reader.nextInt();
+                    if ( (testSize < 1) || (testSize > originalFullTestSize) )
+                        testSize = DEFAULT_TEST_SIZE_MAX;
+                } catch (InputMismatchException e) {
+                    testSize = DEFAULT_TEST_SIZE_MAX;
+                }
+            }
+        }
+
+        // Make sure testSize is valid
+        String errorMsg = "Somehow an invalid testSize (" + testSize + ") has been selected. Should be in the range 1.." + originalFullTestSize;
+        assert ( (testSize >= 1) && (testSize <= originalFullTestSize) ) : errorMsg;
+
+        // Grab random entries to test if not testing the whole set
+        if (testSize == originalFullTestSize)
+            fullTestList = originalFullTestList;
+        else {
+            size = failureList.size();
+            while (testSize < size) {
+                randIndex = rand.nextInt(size);
+                failureList.remove(randIndex);
+                size--;
+            }
+        }
+        trimmedFailureList = new ArrayList<String>(failureList);
+
+        // The main loop routine
+        do {
+        failureList = new ArrayList<String>(trimmedFailureList);
         while ( (failureList != null) && (size = failureList.size()) > 0) {
             statesThisRound = new ArrayList<>(failureList);
             while (size>0) {
@@ -61,13 +118,17 @@ public class simpleTester {
             }
             size = failureList.size();
             if (size>0) {
-                System.out.println("You have " + size + " States left to get correct. Wanna play again?");
-                System.out.print("yes or no (will default to yes): ");
-                String repeat = reader.nextLine();
+                System.out.println("You have " + size + " prompts left to get correct. Wanna play again?");
+                System.out.print("  yes or no (will default to yes): ");
+                repeat = reader.nextLine();
                 if (repeat.toLowerCase().equals("no"))
                     failureList = null;
             }
         }
+            System.out.println("Do you want to repeat this exam?");
+            System.out.print("  yes or no (will default to no): ");
+            repeat = reader.nextLine();
+        } while (repeat.toLowerCase().equals("yes"));
         
         reader.close();
     }
